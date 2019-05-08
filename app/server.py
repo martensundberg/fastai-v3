bed78af7e95b60a9b1c9d7d35d327eb51bd71d63
Mårten Sundberg <marten.sundberg@ericsson.com>
2019-05-08 12:16:38 +0200

    Added print statements for debug

--- app/server.py
+++ app/server.py
@@ -29,6 +29,8 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
     try:
+        print('Path:',path)
+        print('Export file name:',export_file_name)        
         learn = load_learner(path, export_file_name)
         return learn
     except RuntimeError as e:
